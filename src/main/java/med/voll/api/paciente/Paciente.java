908df52d4006c45f25b35778522dae67625ac287
Lucas <lksbrs@gmail.com>
2023-01-25 13:35:50 -0300

    Alteração e exclução de médicos e pacientes

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -22,6 +22,7 @@ public class Paciente {
     private String email;
     private String telefone;
     private String cpf;
+    private Boolean ativo;
 
     @Embedded
     private Endereco endereco;
@@ -32,5 +33,22 @@ public class Paciente {
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = Boolean.TRUE;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizaInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = Boolean.FALSE;
     }
 }
